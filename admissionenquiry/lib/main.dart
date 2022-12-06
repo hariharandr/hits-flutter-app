ae8915b9c79d879014157bbdfb75571e0b252fc8
Hariharan <hariravidhivya@gmail.com>
2022-12-06 11:50:26 +0530

    videoplayer plugin error

--- admissionenquiry/lib/main.dart
+++ admissionenquiry/lib/main.dart
@@ -1,173 +1,3 @@
-// import 'dart:io';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-
-// Future main() async {
-//   //If you're running an application and need to access the binary messenger before runApp() has been called (for example, during plugin initialization), then you need to explicitly call the
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   if (Platform.isAndroid) {
-//     await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
-//   }
-
-//   runApp(const VideoDetails());
-// }
-
-// class VideoDetails extends StatefulWidget {
-//   const VideoDetails({Key? key}) : super(key: key);
-
-//   @override
-//   State<VideoDetails> createState() => _VideoDetailsState();
-// }
-
-// class _VideoDetailsState extends State<VideoDetails> {
-//   late InAppWebViewController webView;
-//   InAppWebViewController? webViewController;
-//   final GlobalKey webViewKey = GlobalKey();
-
-//   final InAppWebViewGroupOptions _options = InAppWebViewGroupOptions(
-//       crossPlatform: InAppWebViewOptions(
-//         useShouldOverrideUrlLoading: true,
-//         mediaPlaybackRequiresUserGesture: false,
-//       ),
-//       android: AndroidInAppWebViewOptions(
-//         useHybridComposition: true,
-//       ),
-//       ios: IOSInAppWebViewOptions(
-//         allowsInlineMediaPlayback: true,
-//       ));
-
-//   late PullToRefreshController pullToRefreshController;
-//   String url = "";
-//   double progress = 0;
-//   final urlController = TextEditingController();
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     pullToRefreshController = PullToRefreshController(
-//       options: PullToRefreshOptions(
-//         color: Colors.blue,
-//       ),
-//       onRefresh: () async {
-//         if (Platform.isAndroid) {
-//           webViewController?.reload();
-//         } else if (Platform.isIOS) {
-//           webViewController?.loadUrl(
-//               urlRequest: URLRequest(url: await webViewController?.getUrl()));
-//         }
-//       },
-//     );
-//   }
-
-//   @override
-//   void dispose() {
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//           appBar: AppBar(title: const Text("Some Random Title")),
-//           body: SafeArea(
-//               child: Column(children: <Widget>[
-//             Expanded(
-//               child: Stack(
-//                 children: [
-//                   InAppWebView(
-//                     key: webViewKey,
-//                     initialUrlRequest: URLRequest(
-//                       url: Uri.parse(
-//                           "https://drive.google.com/file/d/1vwLAljWAYCbaBWXGNhxFH5UBYLml-8VZ/view?usp=sharing"),
-//                     ),
-//                     initialOptions: _options,
-//                     pullToRefreshController: pullToRefreshController,
-//                     onWebViewCreated: (controller) {
-//                       webViewController = controller;
-//                     },
-//                     onLoadStart: (controller, url) {
-//                       setState(() {
-//                         this.url = url.toString();
-//                         urlController.text = this.url;
-//                       });
-//                     },
-//                     androidOnPermissionRequest:
-//                         (controller, origin, resources) async {
-//                       return PermissionRequestResponse(
-//                           resources: resources,
-//                           action: PermissionRequestResponseAction.GRANT);
-//                     },
-//                     shouldOverrideUrlLoading:
-//                         (controller, navigationAction) async {
-//                       // var uri = navigationAction.request.url!;
-
-//                       return NavigationActionPolicy.ALLOW;
-//                     },
-//                     onLoadStop: (controller, url) async {
-//                       pullToRefreshController.endRefreshing();
-//                       setState(() {
-//                         this.url = url.toString();
-//                         urlController.text = this.url;
-//                       });
-//                     },
-//                     onLoadError: (controller, url, code, message) {
-//                       pullToRefreshController.endRefreshing();
-//                     },
-//                     onProgressChanged: (controller, progress) {
-//                       if (progress == 100) {
-//                         pullToRefreshController.endRefreshing();
-//                       }
-//                       setState(() {
-//                         this.progress = progress / 100;
-//                         urlController.text = this.url;
-//                       });
-//                     },
-//                     onUpdateVisitedHistory: (controller, url, androidIsReload) {
-//                       setState(() {
-//                         this.url = url.toString();
-//                         urlController.text = this.url;
-//                       });
-//                     },
-//                     onConsoleMessage: (controller, consoleMessage) {
-//                       print(consoleMessage);
-//                     },
-//                   ),
-//                   progress < 1.0
-//                       ? LinearProgressIndicator(value: progress)
-//                       : Container(),
-//                 ],
-//               ),
-//             ),
-//             ButtonBar(
-//               alignment: MainAxisAlignment.center,
-//               children: <Widget>[
-//                 ElevatedButton(
-//                   child: const Icon(Icons.arrow_back),
-//                   onPressed: () {
-//                     webViewController?.goBack();
-//                   },
-//                 ),
-//                 ElevatedButton(
-//                   child: const Icon(Icons.arrow_forward),
-//                   onPressed: () {
-//                     webViewController?.goForward();
-//                   },
-//                 ),
-//                 ElevatedButton(
-//                   child: const Icon(Icons.refresh),
-//                   onPressed: () {
-//                     webViewController?.reload();
-//                   },
-//                 ),
-//               ],
-//             ),
-//           ]))),
-//     );
-//   }
-// }
-
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -206,7 +36,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     // offers several different constructors to play videos from assets, files,
     // or the internet.
     _controller = VideoPlayerController.network(
-      'https://drive.google.com/file/d/1vwLAljWAYCbaBWXGNhxFH5UBYLml-8VZ/view',
+      'https://raw.githubusercontent.com/Abdulhaseeb101/hindustan-addmission-app-resources/main/videos/Mr.Dennis.mp4',
     );
 
     // Initialize the controller and store the Future for later use.
@@ -228,7 +58,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Butterfly Video'),
+        title: const Text('Sample Video'),
       ),
       // Use a FutureBuilder to display a loading spinner while waiting for the
       // VideoPlayerController to finish initializing.
